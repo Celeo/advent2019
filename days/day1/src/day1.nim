e8da7da8b7da9fb8a0fd30ad56390f32d5d277fc
Celeo <celeodor@gmail.com>
2019-12-01 15:45:22 -0800

    day 1 part 2

--- days/day1/src/day1.nim
+++ days/day1/src/day1.nim
@@ -1,8 +1,24 @@
-import math, sequtils, strutils, sugar
+import math, sequtils, strutils
 
-proc part1(masses: seq[string]): int =
-  foldl(map(masses, (m) => math.floor(m.parseInt() / 3).toInt() - 2), a + b)
+proc fuelCost(mass: int): int =
+  ## Calculate the fuel requirements for the mass.
+  floor(mass / 3).toInt() - 2
+
+proc part1(masses: seq[int]): int =
+  ## Calculate the fuel cost for all masses in the sequence and sum.
+  masses
+    .map(fuelCost)
+    .foldl(a + b)
+
+proc part2(masses: seq[int]): int =
+  ## Calculate the fuel cost for all masses and the fuel therein, and sum.
+  for mass in masses:
+    var fuel = fuelCost(mass)
+    while fuel > 0:
+      result += fuel
+      fuel = fuelCost(fuel)
 
 when isMainModule:
-  let masses = readFile("input.txt").strip().splitLines()
-  echo("Total cost of fuel: " & $part1(masses))
+  let masses = readFile("input.txt").strip().splitLines().map(parseInt)
+  echo("Part 1: " & $part1(masses))
+  echo("Part 2: " & $part2(masses))
