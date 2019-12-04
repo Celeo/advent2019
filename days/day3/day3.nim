c2c3f82f13a79d7b90099254f272d3057519a2e4
Celeo <celeodor@gmail.com>
2019-12-03 18:03:51 -0800

    day 3 part 2

--- days/day3/day3.nim
+++ days/day3/day3.nim
@@ -1,14 +1,33 @@
-import math, sequtils, sets, strutils, sugar
+import hashes, math, sequtils, sets, strutils
 
-proc distanceTo(a: (int, int)): int =
-  ## Calculate the distance between the point and the origin.
-  abs(a[0]) + abs(a[1])
+type
+  Point = object
+    x: int
+    y: int
+    steps: int
 
-proc layWire(directions: seq[string]): HashSet[(int, int)] =
+proc newPoint(x, y, steps: int): Point =
+  ## Create a new point object instance.
+  Point(x: x, y: y, steps: steps)
+
+proc `==`(a, b: Point): bool =
+  ## Compare two points for equality.
+  ##
+  ## Note that the step count is NOT included in this test.
+  a.x == b.x and a.y == b.y
+
+proc hash(p: Point): Hash =
+  ## Hash a point instance.
+  ##
+  ## Note that the step count is NOT included in the hash.
+  !$(0 !& p.x !& p.y)
+
+proc layWire(directions: seq[string]): HashSet[Point] =
   ## Return a seq of all the points the wire touches.
   var
     x = 0
     y = 0
+    steps = 1
   for direction in directions:
     let
       d = direction[0]
@@ -34,19 +53,30 @@ proc layWire(directions: seq[string]): HashSet[(int, int)] =
         quit(1)
     while xMod > 0:
       x += xMult
-      result.incl((x, y))
+      let p = newPoint(x, y, steps)
+      if p notin result:
+        result.incl(p)
       xMod.dec()
+      steps.inc()
     while yMod > 0:
       y += yMult
-      result.incl((x, y))
+      let p = newPoint(x, y, steps)
+      if p notin result:
+        result.incl(p)
       yMod.dec()
+      steps.inc()
 
 when isMainModule:
-  let directions = readFile("input.txt").strip().splitLines().map(line => line.split(","))
+  let directions = readFile("input.txt").strip().splitLines().mapIt(it.split(","))
   let firstPoints = layWire(directions[0])
   let secondPoints = layWire(directions[1])
-  var least = 2 ^ 32
+  var
+    leastByDistance = 2 ^ 32
+    leastBySteps = 2 ^ 23
   for point in firstPoints:
     if point in secondPoints:
-      least = min(least, distanceTo(point))
-  echo(least)
+      leastByDistance = min(leastByDistance, abs(point.x) + abs(point.y))
+      let other = secondPoints.toSeq().filterIt(it == point)[0]
+      leastBySteps = min(leastBySteps, point.steps + other.steps)
+  echo("Part 1: ", leastByDistance)
+  echo("Part 2: ", leastBySteps)
