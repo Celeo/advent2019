26b2441305d972499068cddbfd7aed9177654ca8
Celeo <celeodor@gmail.com>
2019-12-04 16:17:11 -0800

    day 4 part 2

--- days/day4/day4.nim
+++ days/day4/day4.nim
@@ -1,43 +1,32 @@
 import strutils, sequtils
 
 proc valid(n: int, allowMoreThanTwo = true): bool =
-  ## Determines if a number is valid.
-  ##
-  ## Note: don't need to check the length or boundies; that's handled by the input and loop.
+  ## Determines if the number is valid.
   result = true
   let s = $n
-  var
-    last = ' '
-    sameTrain = 0
-    hasTrain = false
-  for c in s:
-    if c < last:
-      return false
-    if c == last:
-      sameTrain.inc()
-      if sameTrain == 2:
-        hasTrain = true
-      elif sameTrain > 2 and not allowMoreThanTwo:
-        hasTrain = false
-        sameTrain = 0
-        last = ' '
-    last = c
-  if not hasTrain:
+  if s.len() != 6:
+    return false
+  if s[0] > s[1] or s[1] > s[2] or s[2] > s[3] or s[3] > s[4] or s[4] > s[5]:
+    return false
+  var found = false
+  for i in 0..9:
+    if allowMoreThanTwo:
+      if ($i & $i) in s:
+        found = true
+        break
+    else:
+      if ($i & $i) in s and ($i & $i & $i) notin s:
+        found = true
+        break
+  if not found:
     return false
 
 when isMainModule:
-  doAssert(valid(112233) == true)
-  doAssert(valid(123444) == true)
-  doAssert(valid(111122) == true)
-  doAssert(valid(112233, false) == true)
-  doAssert(valid(123444, false) == false)
-  doAssert(valid(111122, false) == true)
-
   let rangeParts = readFile("input.txt").strip().split("-")
   let
     rangeLow = rangeParts[0].parseInt()
     rangeHigh = rangeParts[1].parseInt()
   let validCountPart1 = (rangeLow..rangeHigh).toSeq().filterIt(valid(it)).len()
-  echo("Part 1: " & $validCountPart1) # should be 1660
+  echo("Part 1: " & $validCountPart1)
   let validCountPart2 = (rangeLow..rangeHigh).toSeq().filterIt(valid(it, false)).len()
-  echo("Part 2: " & $validCountPart2) # FIXME not 280 or 1230
+  echo("Part 2: " & $validCountPart2)
