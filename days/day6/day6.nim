b8bd4bc137e4e6c286c02bc1da0146c719bc2c7f
Celeo <celeodor@gmail.com>
2019-12-06 16:15:02 -0800

    day 6 part 1 progress

--- days/day6/day6.nim
+++ days/day6/day6.nim
@@ -1,6 +1,16 @@
-import strutils
-# https://github.com/erhlee-bird/simple_graph
-import simple_graph
+import sequtils, strformat, strutils
+import simple_graph # https://github.com/erhlee-bird/simple_graph
+
+proc traverse(node: string, graph: DirectedGraph, length = 0) =
+  let newLength = length + 1
+  let orbiters = graph.edges().filterIt(it[0] == node).mapIt(it[1])
+  if orbiters.len() == 0:
+    return
+  echo(&"Orbiting {node} at distance {1} is/are: {orbiters}")
+  for orbiter in orbiters:
+    traverse(orbiter, graph, newLength)
+  # TODO need to make use of 'newLength' to track all of the distances
+  echo(newLength)
 
 when isMainModule:
   let data = readFile("input.txt").strip().splitLines()
@@ -17,4 +27,16 @@ when isMainModule:
       graph.addNode(orbiter)
     graph.addEdge(source, orbiter)
   echo("Number of direct edges: ", graph.edges().len())
+
   # TODO need number of both direct edges and _indirect_ edges
+
+  #[
+    Pick the "COM" point
+    From that point, loop over its edges
+    For each point connected via an edge, that point is 1 away
+    Loop over those points' edges to find which points are connected to them
+    Continue, building a length from the first point to each other point
+    Sum all of those lengths
+  ]#
+
+  traverse("COM", graph)
