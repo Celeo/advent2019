91b8a87325836144bace892635cab9b91800f666
Celeo <celeodor@gmail.com>
2019-12-06 21:00:52 -0800

    day 6 part 2 (wip)

--- days/day6/day6.nim
+++ days/day6/day6.nim
@@ -1,17 +1,27 @@
-import sequtils, strutils
+import sequtils, strformat, strutils, tables
 import simple_graph # https://github.com/erhlee-bird/simple_graph
 
-proc traverse(node: string, graph: DirectedGraph, distances: var seq[int] = @[], length = 0) =
+proc findOutward(graph: DirectedGraph, node: string, distances: var TableRef[string, int], length = 0) =
   let orbiters = graph.edges().filterIt(it[0] == node).mapIt(it[1])
-  distances.add(length)
+  distances[node] = length
   if orbiters.len() == 0:
     return
   for orbiter in orbiters:
-    traverse(orbiter, graph, distances, length + 1)
+    findOutward(graph, orbiter, distances, length + 1)
+
+# proc findBetween(graph: DirectedGraph, a: string, b: string, distances: var TableRef[string, int], length = 0) =
+#   for (left, right) in graph.edges().filterIt(it[0] == a or it[1] == a).mapIt((it[0], it[1])):
+#     echo(left, " ", right)
+#   distances[a] = length
+#   if orbiters.len() <= 1:
+#     return
+#   for orbiter in orbiters:
+#     findBetween(graph, orbiter, b, distances, length + 1)
 
 when isMainModule:
-  let data = readFile("input.txt").strip().splitLines()
-  let graph = DirectedGraph[string]()
+  let
+    data = readFile("input.txt").strip().splitLines()
+    graph = DirectedGraph[string]()
   graph.initGraph()
   for line in data:
     let parts = line.split(")")
@@ -24,7 +34,20 @@ when isMainModule:
       graph.addNode(orbiter)
     graph.addEdge(source, orbiter)
 
-  var distances: seq[int] = @[]
-  traverse("COM", graph, distances)
-  let total: int = distances.foldl(a + b)
-  echo("Part 1: ", total)
+  block:
+    var distances = newTable[string, int]()
+    findOutward(graph, "COM", distances)
+    var distanceValues: seq[int] = @[]
+    for (k, v) in distances.pairs():
+      echo(&"{k}: {v}")
+      distanceValues.add(v)
+    echo("Part 1: ", distanceValues.foldl(a + b))
+
+  # block:
+  #   var distances = newTable[string, int]()
+  #   findBetween(graph, "SAN", "YOU", distances)
+  #   var distanceValues: seq[int] = @[]
+  #   for (k, v) in distances.pairs():
+  #     echo(&"{k}: {v}")
+  #     distanceValues.add(v)
+  #   echo("Part 2: ", distanceValues.foldl(a + b))
