d363cd7806d6e97bb1b2193779b36559d6e80c52
Celeo <celeodor@gmail.com>
2019-12-06 21:35:42 -0800

    day 5 part 2

--- days/day5/day5.nim
+++ days/day5/day5.nim
@@ -3,43 +3,6 @@ import algorithm, math,
   sequtils, strformat,
   strutils, tables
 
-#[
-  -------------------------------------------
-  Opcode    Parameters   Functionality
-  -------------------------------------------
-  1         3            Add arg 1 and 2, store result in arg 3
-  2         3            Multiply arg 1 and 2, store result in arg 3
-  3         1            Takes 1 value from input and saves it in arg 1
-  4         1            Outputs the value in arg 1
-  5         2            If (arg 1 != 0), then sets the position to arg 2
-  6         2            If (arg 1 == 0), then sets the position to arg 2
-  7         3            Saves 1 in arg 3 if (arg 1 < arg 2) else saves 0 to arg 3
-  8         3            Saves 1 in arg 3 if (arg 1 == arg 2) else saves 0 to arg 3
-  99        None         Halt
-
-
-  -------------------------------------------------------------
-  Parameter Mode    Name            Functionality
-  -------------------------------------------------------------
-  0                 Position        Uses the value of the cell in the opcode list at the argument's positon
-  1                 Immediate       Uses the value of the argument
-
-
-  --------------------------------------
-  Instruction Breakdown
-  --------------------------------------
-
-    ABCDE
-    1002
-
-    DE - two-digit opcode,      02 == opcode 2
-    C - mode of 1st parameter,  0 == position mode
-    B - mode of 2nd parameter,  1 == immediate mode
-    A - mode of 3rd parameter,  0 == position mode, omitted due to being a leading zero
-
-    Length will be dependent on the number of arguments for the operation
-]#
-
 const argumentCount = {
   1: 3,
   2: 3,
@@ -51,112 +14,115 @@ const argumentCount = {
   8: 3,
   99: 0
 }.toTable()
-const argModePosition = 0
-const argModeImmediate = 1
 
 type
+  ArgMode = enum
+    ## Represent the two types of argument modes.
+    amPosition = 0
+    amImmediate = 1
+
   Opcode = object
+    ## Represent all the parts of a complex opcode.
     code: int
     argCount: int
-    argModes: seq[int]
+    argModes: seq[ArgMode]
 
-proc newOpcode(code: int, argCount: int, argModes: seq[int]): Opcode =
+proc newOpcode(code: int, argCount: int, argModes: seq[ArgMode]): Opcode =
+  ## Create a new Opcode object.
   Opcode(code: code, argCount: argCount, argModes: argModes)
 
-proc `$`(o: Opcode): string =
-  &"Opcode: code = {o.code}, argCount = {o.argCount}, argModes = {o.argModes}"
-
 proc parseOpcode(code: int): Opcode =
-  debug(&"Parsing opcode of {code}")
+  ## Parse the complex opcode into its individual components.
   var s = $code
   while s.len() < 4:
     s = "0" & s
   let opcode = s[^2..^1].join("").parseInt()
-  var argModes = s[0..^3].toSeq().reversed().mapIt(($it).parseInt())
+  var argModes = s[0..^3].toSeq().reversed().mapIt(ArgMode(($it).parseInt()))
   let argCount = argumentCount[opcode]
   while argModes.len() < argCount:
-    # debug("Adding padding 0 to opcode argModes")
-    argModes.add(argModePosition)
-  result = newOpcode(opcode, argCount, argModes)
-  debug(result)
+    argModes.add(amPosition)
+  newOpcode(opcode, argCount, argModes)
+
+proc getAt(codes: seq[int], position: int, offset: int, argMode: ArgMode): int =
+  if argMode == amImmediate:
+    codes[position + offset]
+  else:
+    codes[codes[position + offset]]
 
 proc processSingle(codes: var seq[int], position: int): int =
-  debug(&"processSingle(), position = {position}")
+  ## Process a single instruction.
+  ##
+  ## **Returns**: the new position
+  debug(&"New processor pass, position is {position}")
   let code = codes[position]
   let opcode = parseOpcode(code)
   let args = codes[(position + 1)..(position + opcode.argCount)]
   if opcode.code == 99:
     return -1
   elif opcode.code == 1:
-    debug("opcode.code == 1")
+    debug("opcode.code is 1")
     let
-      arg1 = if opcode.argModes[0] == argModeImmediate: codes[position + 1] else: codes[codes[position + 1]]
-      arg2 = if opcode.argModes[1] == argModeImmediate: codes[position + 2] else: codes[codes[position + 2]]
+      arg1 = getAt(codes, position, 1, opcode.argModes[0])
+      arg2 = getAt(codes, position, 2, opcode.argModes[1])
     codes[codes[position + 3]] = arg1 + arg2
   elif opcode.code == 2:
-    debug("opcode.code == 2")
+    debug("opcode.code is 2")
     let
-      arg1 = if opcode.argModes[0] == argModeImmediate: codes[position + 1] else: codes[codes[position + 1]]
-      arg2 = if opcode.argModes[1] == argModeImmediate: codes[position + 2] else: codes[codes[position + 2]]
+      arg1 = getAt(codes, position, 1, opcode.argModes[0])
+      arg2 = getAt(codes, position, 2, opcode.argModes[1])
     codes[codes[position + 3]] = arg1 * arg2
   elif opcode.code == 3:
-    debug("opcode.code == 3")
+    debug("opcode.code is 3")
     let val = readLineFromStdin("Enter value: ").parseInt()
     debug("\n\n")
-    if opcode.argModes[0] == argModeImmediate:
+    if opcode.argModes[0] == amImmediate:
       codes[position + 1] = val
     else:
       codes[codes[position + 1]] = val
   elif opcode.code == 4:
-    debug("opcode.code == 4")
+    debug("opcode.code is 4")
     var val = 0
-    if opcode.argModes[0] == argModeImmediate:
+    if opcode.argModes[0] == amImmediate:
       val = codes[position + 1]
     else:
       val = codes[codes[position + 1]]
     echo(val)
   elif opcode.code == 5:
-    debug("opcode.code == 5")
+    debug("opcode.code is 5")
     let
-      arg1 = if opcode.argModes[0] == argModeImmediate: codes[position + 1] else: codes[codes[position + 1]]
-      arg2 = if opcode.argModes[1] == argModeImmediate: codes[position + 2] else: codes[codes[position + 2]]
+      arg1 = getAt(codes, position, 1, opcode.argModes[0])
+      arg2 = getAt(codes, position, 2, opcode.argModes[1])
     if arg1 != 0:
-      debug(&"Position tracker is now {arg2}")
+      debug(&"Moving position tracker to {arg2}")
       return arg2
   elif opcode.code == 6:
-    debug("opcode.code == 6")
+    debug("opcode.code is 6")
     let
-      arg1 = if opcode.argModes[0] == argModeImmediate: codes[position + 1] else: codes[codes[position + 1]]
-      arg2 = if opcode.argModes[1] == argModeImmediate: codes[position + 2] else: codes[codes[position + 2]]
+      arg1 = getAt(codes, position, 1, opcode.argModes[0])
+      arg2 = getAt(codes, position, 2, opcode.argModes[1])
     if arg1 == 0:
-      debug(&"Position tracker is now {arg2}")
+      debug(&"Moving position tracker to {arg2}")
       return arg2
   elif opcode.code == 7:
-    debug("opcode.code == 7")
+    debug("opcode.code is 7")
     let
-      arg1 = if opcode.argModes[0] == argModeImmediate: codes[position + 1] else: codes[codes[position + 1]]
-      arg2 = if opcode.argModes[1] == argModeImmediate: codes[position + 2] else: codes[codes[position + 2]]
-    let valueToStore = if arg1 < arg2:
-      1
-    else:
-      0
-    if opcode.argModes[2] == argModeImmediate:
+      arg1 = getAt(codes, position, 1, opcode.argModes[0])
+      arg2 = getAt(codes, position, 2, opcode.argModes[1])
+    let valueToStore = if arg1 < arg2: 1 else: 0
+    if opcode.argModes[2] == amImmediate:
       codes[position + 3] = valueToStore
     else:
-      codes[codes[position + 1]] = valueToStore
+      codes[codes[position + 3]] = valueToStore
   elif opcode.code == 8:
-    debug("opcode.code == 8")
+    debug("opcode.code is 8")
     let
-      arg1 = if opcode.argModes[0] == argModeImmediate: codes[position + 1] else: codes[codes[position + 1]]
-      arg2 = if opcode.argModes[1] == argModeImmediate: codes[position + 2] else: codes[codes[position + 2]]
-    let valueToStore = if arg1 == arg2:
-      1
-    else:
-      0
-    if opcode.argModes[2] == argModeImmediate:
+      arg1 = getAt(codes, position, 1, opcode.argModes[0])
+      arg2 = getAt(codes, position, 2, opcode.argModes[1])
+    let valueToStore = if arg1 == arg2: 1 else: 0
+    if opcode.argModes[2] == amImmediate:
       codes[position + 3] = valueToStore
     else:
-      codes[codes[position + 1]] = valueToStore
+      codes[codes[position + 3]] = valueToStore
   else:
     error(&"Unknown opcode {opcode.code}")
     quit(1)
@@ -165,6 +131,7 @@ proc processSingle(codes: var seq[int], position: int): int =
   position + positionAdvance
 
 proc processAll(codes: seq[int]): seq[int] =
+  ## Process the entire sequence of codes.
   var
     codesMut = codes
     position = 0
@@ -173,8 +140,6 @@ proc processAll(codes: seq[int]): seq[int] =
   codesMut
 
 when isMainModule:
-  addHandler(newConsoleLogger(levelThreshold = lvlDebug))
+  addHandler(newConsoleLogger(levelThreshold = lvlInfo))
   let codes = readFile("input.txt").strip().split(",").map(parseInt)
   discard processAll(codes)
-  # 10762683 is too low for part 2
-  # FIXME some of the examples for part 2 fail
