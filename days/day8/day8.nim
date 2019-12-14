9c1ebda8db61630d9b510381fb16dc1623c82666
Celeo <celeodor@gmail.com>
2019-12-13 20:07:22 -0800

    day 8 part 2

--- days/day8/day8.nim
+++ days/day8/day8.nim
@@ -6,16 +6,32 @@ when isMainModule:
     width = 25
     height = 6
   let perLayer = width * height
-  var
-    layerWithFewest: seq[char] = @[]
-    fewest = 2 ^ 32
+
+  var layers: seq[string] = @[]
   for i in 0..<(data.len() / perLayer).floor().toInt():
-    let
-      s = i * perLayer
-      e = (i + 1) * perLayer - 1
-    let layer = data[s..e]
-    let zeros = layer.count('0')
-    if zeros < fewest:
-      layerWithFewest = layer[0..^1].toSeq()
-      fewest = zeros
-  echo(layerWithFewest.count('1') * layerWithFewest.count('2'))
+    layers.add(data[(i * perLayer)..((i + 1) * perLayer - 1)])
+
+  block:
+    var
+      layerWithFewest: seq[char] = @[]
+      fewest = 2 ^ 32
+    for layer in layers:
+      let zeroes = layer.count('0')
+      if zeroes < fewest:
+        layerWithFewest = layer[0..^1].toSeq()
+        fewest = zeroes
+    echo("Part 1: ", layerWithFewest.count('1') * layerWithFewest.count('2'))
+
+  block:
+    var image = layers[0]
+    for layer in layers[1..^1]:
+      var replacement = ""
+      for pixel in 0..<perLayer:
+        if image[pixel] == '2':
+          replacement &= layer[pixel]
+        else:
+          replacement &= image[pixel]
+      image = replacement
+    echo("Part 2:")
+    for i in 0..<height:
+      echo(image[(i * width)..(i + 1) * width - 1].replace('0', ' ').replace('1', 'x'))
