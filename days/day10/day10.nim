379735ef22546c00d938b47239b96dd7236c47f4
Celeo <celeodor@gmail.com>
2019-12-14 21:36:28 -0800

    day 10 part 1 progress

--- days/day10/day10.nim
+++ days/day10/day10.nim
@@ -1,21 +1,30 @@
-import strformat, strutils
+import math, sets, strutils
 
-#[
-  points = ...
-  for potential in points
-      visible = 0
-      for other in points
-          if potetial is other
-              skip
-          slope = get_slope(potential, other)
-          if points_on_line(potential, slope) > 1
-              TODO
-          visible ++
-      echo {potential} can see {visible}
-]#
+proc selectMonitoringPoint(points: seq[(int, int)]): (int, (int, int)) =
+  var
+    maxAsteroids = 0
+    bestPoint = (0, 0)
+  for (x, y) in points:
+    var slopes = initHashSet[(int, int)]()
+    for (x2, y2) in points:
+      if x != x2 or y != y2:
+        var
+          dx = x2 - x
+          dy = y2 - y
+        dx = (dx / gcd(dx, dy)).floor().toInt()
+        dy = (dy / gcd(dx, dy)).floor().toInt()
+        slopes.incl((dx, dy))
+    if slopes.len() > maxAsteroids:
+      maxAsteroids = slopes.len()
+      bestPoint = (x, y)
+  (maxAsteroids, bestPoint)
 
-let data = readFile("input.txt").strip().splitLines()
-var points: seq[(int, int, bool)] = @[]
-for x in 0..<data[0].len():
+when isMainModule:
+  let data = readFile("input.txt").strip().splitLines()
+  var points: seq[(int, int)] = @[]
+  for x in 0..<data[0].len():
     for y in 0..<data.len():
-        points.add((x, y, data[x][y] == '#'))
+      if data[x][y] == '#':
+        points.add((y, x))
+
+  echo(selectMonitoringPoint(points))
