5c19e4b03216231073631d90f59188cd5f24339c
Celeo <celeodor@gmail.com>
2019-12-15 15:41:26 -0800

    day 9 refactoring

--- days/day9/day9.nim
+++ days/day9/day9.nim
@@ -27,10 +27,17 @@ type
     argCount: int
     argModes: seq[ArgMode]
 
+  Machine* = ref object
+    codes: seq[int]
+    position: int
+    relativeBase: int
+
 proc newOpcode(code: int, argCount: int, argModes: seq[ArgMode]): Opcode =
+  ## Create a new `Opcode` object.
   Opcode(code: code, argCount: argCount, argModes: argModes)
 
 proc parseOpcode(code: int): Opcode =
+  ## Parse an instruction into an `Opcode` object for easiser reference.
   var s = $code
   while s.len() < 4:
     s = "0" & s
@@ -41,119 +48,127 @@ proc parseOpcode(code: int): Opcode =
     argModes.add(amPosition)
   newOpcode(opcode, argCount, argModes)
 
-proc getAt(codes: seq[int], position: int, relativeBase: int, offset: int, argMode: ArgMode): int =
-  debug(&"getAt(), position = {position}, relativeBase = {relativeBase}, offset = {offset}, argMode = {argMode}")
+proc getAt(m: Machine, offset: int, argMode: ArgMode): int =
+  ## Retrieve a value in the instruction set, following the different argument lookup modes.
+  debug(&"getAt(), position = {m.position}, relativeBase = {m.relativeBase}, offset = {offset}, argMode = {argMode}")
   if argMode == amPosition:
-    codes[codes[position + offset]]
+    m.codes[m.codes[m.position + offset]]
   elif argMode == amImmediate:
-    codes[position + offset]
+    m.codes[m.position + offset]
   elif argMode == amRelative:
-    codes[codes[position + offset] + relativeBase]
+    m.codes[m.codes[m.position + offset] + m.relativeBase]
   else:
     error(&"getAt(): unknown arg mode {argMode}")
     quit(1)
 
-proc setAt(codes: var seq[int], position: int, relativeBase: int, offset: int, argMode: ArgMode, value: int) =
+proc setAt(m: Machine, offset: int, argMode: ArgMode, value: int) =
+  ## Set a value in the instruction set, following the different argument lookup modes.
+  ##
+  ## Note that `amImmediate` is invalid for setting values.
   if argMode == amPosition:
-    codes[codes[position + offset]] = value
+    m.codes[m.codes[m.position + offset]] = value
   elif argMode == amImmediate:
     error("Writing to a vaue in immediate mode is invalid")
     quit(1)
   elif argMode == amRelative:
-    codes[codes[position + offset] + relativeBase] = value
+    m.codes[m.codes[m.position + offset] + m.relativeBase] = value
   else:
     error(&"setAt(): unknown arg mode {argMode}")
     quit(1)
 
-proc processSingle(codes: var seq[int], position: var int, relativeBase: var int) =
-  debug(&"New processor pass, position is {position}")
-  let code = codes[position]
+proc processSingle(m: Machine) =
+  ## Perform a single iteration of processing, handling a single instruction.
+  debug(&"New processor pass, position is {m.position}")
+  let code = m.codes[m.position]
   let opcode = parseOpcode(code)
-  let args = codes[(position + 1)..(position + opcode.argCount)]
+  let args = m.codes[(m.position + 1)..(m.position + opcode.argCount)]
   if opcode.code == 99:
-    position = -1
+    m.position = -1
     return
   elif opcode.code == 1:
     debug("opcode.code is 1")
     let
-      arg1 = getAt(codes, position, relativeBase, 1, opcode.argModes[0])
-      arg2 = getAt(codes, position, relativeBase, 2, opcode.argModes[1])
-    setAt(codes, position, relativeBase, 3, opcode.argModes[2], arg1 + arg2)
+      arg1 = getAt(m, 1, opcode.argModes[0])
+      arg2 = getAt(m, 2, opcode.argModes[1])
+    setAt(m, 3, opcode.argModes[2], arg1 + arg2)
   elif opcode.code == 2:
     debug("opcode.code is 2")
     let
-      arg1 = getAt(codes, position, relativeBase, 1, opcode.argModes[0])
-      arg2 = getAt(codes, position, relativeBase, 2, opcode.argModes[1])
-    setAt(codes, position, relativeBase, 3, opcode.argModes[2], arg1 * arg2)
+      arg1 = getAt(m, 1, opcode.argModes[0])
+      arg2 = getAt(m, 2, opcode.argModes[1])
+    setAt(m, 3, opcode.argModes[2], arg1 * arg2)
   elif opcode.code == 3:
     debug("opcode.code is 3")
     let value = readLineFromStdin("Enter value: ").parseInt()
-    setAt(codes, position, relativeBase, 1, opcode.argModes[0], value)
+    setAt(m, 1, opcode.argModes[0], value)
   elif opcode.code == 4:
     debug("opcode.code is 4")
-    let value = getAt(codes, position, relativeBase, 1, opcode.argModes[0])
+    let value = getAt(m, 1, opcode.argModes[0])
     echo(value)
   elif opcode.code == 5:
     debug("opcode.code is 5")
     let
-      arg1 = getAt(codes, position, relativeBase, 1, opcode.argModes[0])
-      arg2 = getAt(codes, position, relativeBase, 2, opcode.argModes[1])
+      arg1 = getAt(m, 1, opcode.argModes[0])
+      arg2 = getAt(m, 2, opcode.argModes[1])
     if arg1 != 0:
       debug(&"Moving position tracker to {arg2}")
-      position = arg2
+      m.position = arg2
       return
   elif opcode.code == 6:
     debug("opcode.code is 6")
     let
-      arg1 = getAt(codes, position, relativeBase, 1, opcode.argModes[0])
-      arg2 = getAt(codes, position, relativeBase, 2, opcode.argModes[1])
+      arg1 = getAt(m, 1, opcode.argModes[0])
+      arg2 = getAt(m, 2, opcode.argModes[1])
     if arg1 == 0:
       debug(&"Moving position tracker to {arg2}")
-      position = arg2
+      m.position = arg2
       return
   elif opcode.code == 7:
     debug("opcode.code is 7")
     let
-      arg1 = getAt(codes, position, relativeBase, 1, opcode.argModes[0])
-      arg2 = getAt(codes, position, relativeBase, 2, opcode.argModes[1])
+      arg1 = getAt(m, 1, opcode.argModes[0])
+      arg2 = getAt(m, 2, opcode.argModes[1])
     let value = if arg1 < arg2: 1 else: 0
-    setAt(codes, position, relativeBase, 3, opcode.argModes[2], value)
+    setAt(m, 3, opcode.argModes[2], value)
   elif opcode.code == 8:
     debug("opcode.code is 8")
     let
-      arg1 = getAt(codes, position, relativeBase, 1, opcode.argModes[0])
-      arg2 = getAt(codes, position, relativeBase, 2, opcode.argModes[1])
+      arg1 = getAt(m, 1, opcode.argModes[0])
+      arg2 = getAt(m, 2, opcode.argModes[1])
     let value = if arg1 == arg2: 1 else: 0
-    setAt(codes, position, relativeBase, 3, opcode.argModes[2], value)
+    setAt(m, 3, opcode.argModes[2], value)
   elif opcode.code == 9:
     debug("opcode.code is 9")
-    let arg1 = getAt(codes, position, relativeBase, 1, opcode.argModes[0])
-    debug(&"Moving relative base by {arg1} to {relativeBase + arg1}")
-    relativeBase += arg1
+    let arg1 = getAt(m, 1, opcode.argModes[0])
+    debug(&"Moving relative base by {arg1} to {m.relativeBase + arg1}")
+    m.relativeBase += arg1
   else:
     error(&"Unknown opcode {opcode.code}")
     quit(1)
   let positionAdvance = args.len() + 1
   debug(&"Advancing position by {positionAdvance}")
-  position += positionAdvance
+  m.position += positionAdvance
 
-proc processAll(codes: seq[int]): seq[int] =
-  var
-    codesMut = codes
-    position = 0
-    relativeBase = 0
-  while position != -1:
-    processSingle(codesMut, position, relativeBase)
-  codesMut
+proc processLoop(m: Machine) =
+  ## Process all a machine's instructions until it encounters instruction code 99.
+  while m.position != -1:
+    processSingle(m)
 
-proc padMemory(codes: seq[int]): seq[int] =
+proc loadMachine(codes: seq[int]): Machine =
+  ## Load a sequence of instruction codes into a new machine object.
+  ##
+  ## This processing takes into account the requirement for memory padding.
   var longCodes = newSeqUninitialized[int](2 ^ 16)
   for i in 0..<codes.len():
     longCodes[i] = codes[i]
-  longcodes
+  Machine(
+    codes: longcodes,
+    position: 0,
+    relativeBase: 0
+  )
 
 when isMainModule:
   addHandler(newConsoleLogger(levelThreshold = lvlInfo))
   let raw = readFile("input.txt").strip().split(",").map(parseInt)
-  let codes = padMemory(raw)
-  discard processAll(codes)
+  let machine = loadMachine(raw)
+  processLoop(machine)
